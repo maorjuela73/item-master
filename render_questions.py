7ad35d6df42fc702263b9cf28a7898c1721d9efc
Miguel Angel O.R. <ma.orjuela73@gmai.com>
2024-11-06 21:27:51 -0500

    diseño de tablas agregado y feedback en plantilla

--- render_questions.py
+++ render_questions.py
@@ -1,16 +1,35 @@
+from jinja2 import Template, Environment, FileSystemLoader
+import re
+
 from questions.idatos import questions
 
-from jinja2 import Template, Environment, FileSystemLoader
+# Define el filtro personalizado
+def regex_replace(s, pattern, replacement=""):
+    return re.sub(pattern, replacement, s)
+
+# Expresión regular para eliminar signos de interrogación y truncar en el primer punto o signo de cierre de interrogación
+def clean_and_truncate(text):
+    # Eliminamos todos los signos de interrogación
+    text = re.sub(r"\?", "", text)
+    # Truncamos hasta el primer punto o signo de cierre de interrogación
+    match = re.search(r"[.]", text)
+    if match:
+        text = text[:match.start() + 1]  # Incluye el punto en el truncado
+    return text
+
 env = Environment(
     loader=FileSystemLoader('.'),
     trim_blocks=True,
     lstrip_blocks=True)
 
+env.filters['regex_replace'] = regex_replace
+env.filters['clean_and_truncate'] = clean_and_truncate
+
 # Cargar la plantilla Moodle XML
 with open('templates/relational_with_categories.xml', 'r') as file:
     moodle_template_str = file.read()
 
-moodle_template = Template(moodle_template_str)
+moodle_template = env.from_string(moodle_template_str)
 
 # Renderizar el XML completo
 rendered_moodle_xml = moodle_template.render(questions=questions)
